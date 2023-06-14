4de44cd882cb1c411b065de8b70fd96dff864712
Pradip-Gadal <pradip.gadal.ds369@gmail.com>
2023-06-14 07:23:50 +0545

    Classes

--- extra.js
+++ extra.js
@@ -1,7 +1,50 @@
-//from D_P:
-function main(a, b=main()){
-    return a + b;
-};
-main() // output=RangeError: Maximum call stack size exceeded
+
+class Square {
+    constructor(_width){
+        this.width = _width
+        this.heigth = _width
+        this.callTheGet = 0;
+    }
+
+    get Area() {
+        this.callTheGet++;
+        return this.width * this.heigth;
+    }
+
+    set Arearea (Area){
+        this.width = Math.sqrt(Area);
+        this.heigth = this.width;
+    }
+}
+
+let AreaOfSquare = new Square(32);
+console.log(AreaOfSquare.Area)
+
+AreaOfSquare.Arearea = 25;
+console.log(AreaOfSquare.Area)
+console.log(AreaOfSquare.width)
+console.log(AreaOfSquare.heigth)
+//
+console.log(AreaOfSquare.Area)
+console.log(AreaOfSquare.Area)
+console.log(AreaOfSquare.Area)
 
 
+console.log(AreaOfSquare.callTheGet)
+
+//Static Method:
+
+class square1 {
+    constructor(_width) {
+        this.width = _width;
+        this.heigth = _width;
+    }
+    static equall(a, b) {
+        return a.width*a.heigth === b.width*b.heigth?"equall":"not equall";
+
+    }
+}
+
+let square_a = new square1(8);
+let square_b = new square1(8)
+console.log(square1.equall(square_a,square_b))
